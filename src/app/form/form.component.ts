e38b7880f4e611aa0c5fb33c61562be21daa7998
o_alfimov <steamcat089@gmail.com>
2019-07-22 17:01:35 +0300

    color-selector doughter component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -8,19 +8,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class FormComponent implements OnInit {
   public colorForm: FormGroup;
-  palette = ['black', '#f23565', '#3cb4f0', '#35f267', '#855313', '#871b48', '#1f5494', '#21943f'];
-  // palette = ['yellow', 'blue', 'green', 'orange', 'brown'];
-  // palette =
-  //   ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-  //     '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-  //     '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-  //     '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-  //     '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-  //     '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-  //     '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-  //     '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-  //     '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-  //     '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+  @Input()palette: string[] = ['black', '#f23565', '#3cb4f0', '#35f267', '#855313', '#871b48', '#1f5494', '#21943f'];
 
   constructor(private fb: FormBuilder) {
   }
